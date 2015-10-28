59738bd1312ad31860efa8323af9003489be25b4
MattCordier <mcordier24@gmail.com>
2015-10-27 23:42:57 -0500

    added js links and functions to sim and diff page

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
-<script>
 
 
+$(document).ready(function(){
+    $(".fade-right").animate({left:400, opacity:"show"}, 1500);
+});
 
-
-
-$(function(){  // $(document).ready 
-  $('#fadein').fadeIn(3000);
+$(document).ready(function(){
+    $(".fade-left").animate({right:400, opacity:"show"}, 1500);
 });
 
 
-</script>
+
+
+// $(function(){  // $(document).ready 
+//   $('#fadein').fadeIn(3000);
+// });
+
+
+
 
 
